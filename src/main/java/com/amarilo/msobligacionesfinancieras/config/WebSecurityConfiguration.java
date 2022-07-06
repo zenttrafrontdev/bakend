9f76a7e8d3cbc31093b83c6d5a076e2983fc0f34
Jesus D Villalba <jesusvillalbam@gmail.com>
2022-07-05 22:15:18 -0500

    cors config

--- src/main/java/com/amarilo/msobligacionesfinancieras/config/WebSecurityConfiguration.java
+++ src/main/java/com/amarilo/msobligacionesfinancieras/config/WebSecurityConfiguration.java
@@ -1,8 +1,15 @@
 package com.amarilo.msobligacionesfinancieras.config;
 
 import com.azure.spring.cloud.autoconfigure.aad.AadResourceServerWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class WebSecurityConfiguration extends AadResourceServerWebSecurityConfigurerAdapter {
@@ -12,6 +19,8 @@ public class WebSecurityConfiguration extends AadResourceServerWebSecurityConfig
         super.configure(http);
         http
                 .csrf().disable()
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/actuator/**").permitAll()
                         .antMatchers("/swagger-ui.html").permitAll()
@@ -21,6 +30,17 @@ public class WebSecurityConfiguration extends AadResourceServerWebSecurityConfig
                         .antMatchers("/v3/api-docs/swagger-config").permitAll()
                         .anyRequest().authenticated()
                 );
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Origin", "Content-Type", "Accept"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
